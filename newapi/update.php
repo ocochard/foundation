d258d4fc96cb77cfa01ab67ecadbd0383093284d
Spike Spiegel <spikeandfaye@gmail.com>
2017-03-05 18:28:06 +0100

    pass

---
+++ newapi/update.php
@@ -0,0 +1,33 @@
+<?php
+
+require '../vendor/autoload.php';
+require './methods.php';
+
+use GeoIp2\Database\Reader;
+
+try
+{
+   $reader = new Reader('../data/GeoLite2-Country.mmdb');
+
+   $bdd = new PDO('mysql:host=dbserver;dbname=content_delivery_weather;charset=utf8', 'spike', 'valentine');
+   $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   $rest_json = file_get_contents("php://input");
+   $_POST = json_decode($rest_json, true);
+
+   //print_r($_POST);
+
+   /*$tables = gettables($bdd, $reader, $_POST);
+
+   if (count($tables) > 0) {
+      $probe = getprobe($bdd, $reader, $tables, $_POST);
+      if (count($probe) > 0) {
+         geturl($bdd, $reader, $tables, $probe);
+      }
+   }*/
+
+} catch (PDOException $e) {
+   die('Error : ' . $e->getMessage());
+}
+
+?>
