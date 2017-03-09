b2d374439264bb483689c3d48bffe9a9fc307257
Olivier Cochard-Labbé <olivier@cochard.me>
2017-03-09 17:33:22 +0100

    Fix UUOC and constitent POSIX usage

--- scripts/run_tunnel_ssh.sh
+++ scripts/run_tunnel_ssh.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd $HOME/.ssh
-
-for i in `cat config | grep "^Host " | cut -d" " -f2`
+for i in $(grep "^Host " $HOME/.ssh/config | cut -d" " -f2)
 do
    RUNNING=$(ps -aux | grep autossh | grep $i)
    if [ -z "$RUNNING" ];
